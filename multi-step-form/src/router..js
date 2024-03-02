1689a4743fa5ed5897905664d13f914570e7749a
Sonay Kara <zonaykara@gmail.com>
2024-03-02 22:11:17 +0300

    configured-project-dependencies

---
+++ multi-step-form/src/router..js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHashHistory } from "vue-router"
+
+const routes = [
+
+    {
+        path: "",
+        // component 
+
+    }
+]
+
+const router = createRouter({
+    routes : routes,
+    history : createWebHashHistory
+})
+
+export default router
---
+++ multi-step-form/src/store.js
@@ -0,0 +1,13 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+    state : {
+        person : {
+            name :"",
+            lastName : ""
+        }
+    }
+})
+
+
+export default store
